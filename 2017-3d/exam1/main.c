37971d4b7c978a8c77cae6402af9b2f54d9e3a1b
pandada8 <pandada8@gmail.com>
2017-03-27 13:44:46 +0800

    copy code

--- 2017-3d/exam1/main.c
+++ 2017-3d/exam1/main.c
@@ -1,7 +1,7 @@
 /** Test.C **/
 
 #include <GLUT/glut.h>
-
+const float PI = 3.1415;
 void display();
 void init();
 
@@ -18,22 +18,60 @@ int main(int argc, char ** argv)
 	glutMainLoop();
 }
 
+void drawLine() {
+	float t;
+	float x, y, z;
+	float a=2,b=3,c=18;
+	glColor3f(1.0,0.5,0.5);
+	glBegin(GL_LINE_STRIP);
+		for(t=0.0;t<=2*PI;t+=0.0002)
+		{
+			x=a*t*cos(c*t)+b;
+			y=a*t*sin(c*t)+b;
+			z=c*t;
+			glVertex3f(x,y,z);
+		}
+	glEnd();
+
+	glColor3f(1.0,1.0,1.0);
+	glBegin(GL_LINES);  //建立坐标轴
+		glVertex3f(0,0,0);
+		glVertex3f(12,0,0);
+	glEnd();
+	glBegin(GL_LINES);  //建立坐标轴
+         glVertex3f(0,0,0);
+		glVertex3f(0,0,12);
+	glEnd();
+	glBegin(GL_LINES);  //建立坐标轴
+		glVertex3f(0,0,0);
+		glVertex3f(0,12,0);
+	glEnd();
+}
+
 void display()
-{  
+{
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glBegin(GL_POLYGON);
-	glVertex2f(-0.5, -0.5);
-	glVertex2f(-0.5, 0.5);
-	glVertex2f(0.5, 0.5);
-	glVertex2f(0.5, -0.5);
-	glEnd();
+	drawLine();
 
 	glFlush();
 }
 
 void init()
 {
-  glClearColor(0.0,0.0,0.0,0.0);
+	glClearColor(0.0,0.0,0.0,0.0);
+
+	glColor3f(1.0,1.0,1.0);
+
+	gluLookAt(
+		1, 1, 1,   //更改视角
+		3, 3, 3,
+		-1,-1,1
+	);
+
+	glMatrixMode(GL_PROJECTION);  //设置投影模式
+	glLoadIdentity();            //设置单位矩形
+	glOrtho(-12.0,12.0,-12.0,12.0,-12,12);
+
 }
 
